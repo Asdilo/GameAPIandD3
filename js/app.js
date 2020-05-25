c767bc6d1e98b2cdc8f5dfcbc316435b4e389f3f
Asterisk Loftis <asterisk@asteriskloftis.com>
2020-05-24 18:10:14 -0700

    Update app.js
    Added IGDB API, however CORS version is not created.

--- js/app.js
+++ js/app.js
@@ -1,64 +1,47 @@
-function loadData() {
-
+function gameSearch() {
     // remove resultset if this has already been run
     $('#gameoutput .gameoutput').remove();
     // add spinner to indicate something is happening
     $('<i class="fa fa-refresh fa-spin"/>').appendTo('body');
 
     // get search term from input
-    var game = $('#gamename').val().replace(/ /g, '+'); //remove those blasted spaces and add a damn plus signs
+    var game = $('#gamename').val();
 
-    var output = $.ajax({
-        url: 'https://api-2445582011268.apicast.io/games/?fields=*&limit=40&offset=0&order=release_dates.date:asc&search=' + game, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-        contentType: 'text/plain',
-        crossDomain: true,
-        xhrFields: {
-            withCredentials: false
+    var items = []; //make an empty array
+
+
+    var settings = {
+        "url": "https://api-v3.igdb.com/games?search=zelda&fields=name, cover.url,  summary, storyline, url, websites",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "user-key": "d850a044f368de4bdc7905afd9c5dab5",
         },
-        data: {}, // Additional parameters here
-        dataType: 'json',
-        success: function (data) {
-            // do all this on success       
-            var items = []; //make an empty array
+      };
 
-            console.log(data); //just need to check the data in the console so I can see what objects are returned
-
-            $.each(data, function (key, val) {
-                function picCheck() { //check if there's a picId if nah give it the generic image id
-                    var picId;
-                    if (typeof val.cover === 'undefined') {
-                        picId = 'nocover_qhhlj6'
-                    } else {
-                        picId = val.cover.cloudinary_id;
-                    }
-                    return picId;
-                };
-
-                //iterate through the returned data and build a list
-                items.push('<a href="' + val.url + '" target="_blank"><div class="" id="' + key + '"><img src="https://res.cloudinary.com/igdb/image/upload/t_logo_med/' + picCheck() + '.jpg"><span>' + val.name + '</span></li></a>');
-            });
-            // if no items were returned then add a message to that effect
-            if (items.length < 1) {
-                items.push('<li>No results for this Game, try again!</li>');
-            }
-
-            // remove spinner
-            $('.fa-spin').remove();
-
-            // append list to page
-            $div = $('<div class="row row-wrap gameoutput"  />').appendTo('#gameoutput');
-
-            //append list items to list
-            $div.append(items);
-        },
-        error: function (err) {
-            alert(err);
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("user-key", "a4f2207c6b692358532b448a5632cbe6"); // Enter here your Mashape key
-        }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        renderResults(response.results);
     });
+
+    function renderResults(response) {
+        // remove spinner
+        $('.fa-spin').remove();
+
+        // append list to page
+        $div = $('<div class="grid gameoutput"  />').appendTo('#gameoutput');
+
+        $.each(response, function (key, val) {
+            //iterate through the returned data and build a list
+            items.push('<div class="card" id="' + val.id  + '"><img src="' + val.cover.url + '"><p>' + val.name + ' - ' + val.summary + '</p></div>');
+        });
+        // if no items were returned then add a message to that effect
+        if (items.length < 1) {
+            items.push('<div class="card">No results for this Game, try another game title.</div>');
+        }
+        //append list items to list
+        $div.append(items);
+    }
 
 
 }
